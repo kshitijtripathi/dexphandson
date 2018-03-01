040be69054c7b06f9c02b3e443ecc68985bccf0b
kshitijtripathi <kshitij.tripathi@zoho.com>
2018-03-01 23:08:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function isNullOrEmpty(input) {
+
+    // Returns true if the input is either undefined, null, or empty, false otherwise
+    return (input === undefined || input === null || input === '');
+}
